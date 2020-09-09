c01b8dbe1c6a3a59b4dcac1a13617d98e765a3f2
Sebulba Lynell <slynell@mi-c1.msk.21-school.ru>
2020-09-09 19:54:43 +0300

    Add 2

--- src/ls_read.c
+++ src/ls_read.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ls_read.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: air_must <air_must@student.42.fr>          +#+  +:+       +#+        */
+/*   By: slynell <slynell@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/25 14:22:06 by slynell           #+#    #+#             */
-/*   Updated: 2020/09/09 19:05:05 by air_must         ###   ########.fr       */
+/*   Updated: 2020/09/09 19:21:48 by slynell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/ft_ls.h"
 
-int ls_read_opt(t_ls *obj, int ac, char **av)
+int			ls_read_opt(t_ls *obj, int ac, char **av)
 {
 	while (obj->index < ac && *av[obj->index] == '-')
 	{
@@ -34,7 +34,7 @@ int ls_read_opt(t_ls *obj, int ac, char **av)
 	return (1);
 }
 
-t_lst_file *pre_error(char *path_dir, char *name, t_lst_file *lst)
+t_lst_file	*pre_error(char *path_dir, char *name, t_lst_file *lst)
 {
 	ls_lst_free(lst);
 	if (errno == 13)
@@ -49,11 +49,11 @@ t_lst_file *pre_error(char *path_dir, char *name, t_lst_file *lst)
 	return (lst);
 }
 
-t_lst_file *ls_read_file(char *path_dir, char *name,
-						 t_lst_file *lst, t_ls *x)
+t_lst_file	*ls_read_file(char *path_dir, char *name,\
+			t_lst_file *lst, t_ls *x)
 {
-	DIR *dir;
-	struct dirent *entry;
+	DIR				*dir;
+	struct dirent	*entry;
 
 	if ((dir = opendir(path_dir)) == NULL)
 		lst = pre_error(path_dir, name, lst);
@@ -61,30 +61,30 @@ t_lst_file *ls_read_file(char *path_dir, char *name,
 	{
 		while ((entry = readdir(dir)) != NULL)
 		{
-			if ((entry->d_name[0] == '.' && x->opt & A_OPT) || entry->d_name[0] != '.')
-				{
-					lst = (lst) ? ls_lst_add(lst) : ls_lst_create(0);
-					lst->name = ft_strdup(entry->d_name);
-					lst->path = ls_file_concat_path_dir(path_dir, lst->name);
-					lst->error = ft_strdup("Good");
-					if (!(lstat(lst->path, &lst->stat) == 0))
-						lst = pre_error(path_dir, name, lst);
-					if (S_ISDIR(lst->stat.st_mode) && !ls_lst_is_root(lst) &&
-						x->opt & RR_OPT && ((lst->name[0] == '.' && x->opt & A_OPT) || lst->name[0] != '.'))
-						lst->child = ls_read_file(lst->path, lst->name, lst->child, x);
-				}
+			if((entry->d_name[0] == '.' && x->opt & A_OPT) || entry->d_name[0] != '.')
+			{
+			lst = (lst) ? ls_lst_add(lst) : ls_lst_create(0);
+			lst->name = ft_strdup(entry->d_name);
+			lst->path = ls_file_concat_path_dir(path_dir, lst->name);
+			lst->error = ft_strdup("Good");
+			if (!(lstat(lst->path, &lst->stat) == 0))
+				lst = pre_error(path_dir, name, lst);
+			if (S_ISDIR(lst->stat.st_mode) && !ls_lst_is_root(lst) &&
+			x->opt & RR_OPT)
+				lst->child = ls_read_file(lst->path, lst->name, lst->child, x);
+			}
 		}
 		closedir(dir);
 	}
 	return (lst);
 }
 
-int ls_read_lst_file(t_ls *obj, int ac, char **av)
+int			ls_read_lst_file(t_ls *obj, int ac, char **av)
 {
-	t_lst_file *lst;
-	t_lst_file *root;
-	t_lst_file *main;
-	int s;
+	t_lst_file	*lst;
+	t_lst_file	*root;
+	t_lst_file	*main;
+	int			s;
 
 	s = 0;
 	main = ls_lst_create(1);
